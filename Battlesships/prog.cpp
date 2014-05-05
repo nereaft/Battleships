ab063aec44f9492aae01a3224d29a62dac1eb429
Nerea Fernandez Torres <nfernandez@dam1-pc00.(none)>
2014-05-05 10:23:24 +0200

    create table

---
+++ Battlesships/prog.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+Typedef struct{
+    int x;
+    int y;
+}Coordenadas;
+
+Typedef struct{
+    int Portaviones;/*5*/
+    int Submarino;/*3*/
+    int Acorazado;/*4*/
+    int Destructor;/*2*/
+}
+
+
+
+
+
+int main(int argc, char *argv[]){
+
+    return EXIT_SUCCESS;
+}
