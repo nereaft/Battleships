80fcf715460337fc15cdd0c2acdaf549cf2da818
nerea <nerea@ubuntu.(none)>
2014-05-11 14:00:14 +0200

    Change game

--- prog.cpp
+++ prog.cpp
@@ -1,63 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+enum  TipoOpcion {partida, guardadas, cargar, ranking, instrucciones, salir};
 
 
-void titlulo(){
-    system("clear");
-    sytem("figlet ***Hundir_La_Flota***");
+int titlulo(){
+	system("clear");
+	system("figlet ***Hundir_La_Flota***");
 }
 
-menu(opcion){
-    switch(opcion){
-	case 1 :
-	    break;
-	case 2 :
-	    break;
-	case 3 :
-	    break;
-	case 4 :
-	    break; 
-	case 5 :
-	    break;
-	case 6 :
-	    break;
-    }
+enum TipoOpcion menu(){
+	int opcion;
+
+do{
+	titulo();
+        printf(" \n\t Menu del Juego:\n"
+	         "\t\t1. Nueva Partida  \n"
+		 "\t\t2. Partidas Guardadas \n"
+                 "\t\t3. Cargar Partida \n"
+                 "\t\t4. Ranking \n"
+                 "\t\t5. Instrucciones del Juego \n"
+               	 "\t\t6. Salir \n"
+                 "\n Opcion Elegida: ");
+	_fpurge(stdin);
+	scanf(" %i", &opcion);
+}while(opcion<1 || opcion>salir + 1);
+	titulo();
+	return (enum TipoOpcion) (opcion - 1);
 }
-}
-}
-}
+
 
 Typedef struct{
-    int x;
-    int y;
+	int x;
+	int y;
 }Coordenadas;
 
 Typedef struct{
-    int Portaviones;/*5*/
-    int Submarino;/*3*/
-    int Acorazado;/*4*/
-    int Destructor;/*2*/
+	int Portaviones;/*5*/
+	int Submarino;/*3*/
+	int Acorazado;/*4*/
+	int Destructor;/*2*/
 }
 
-
-
-
-
 int main(int argc, char *argv[]){
-    int opcion;
-    titulo();
-    printf(" \n\t Menu del Juego:\n"
-	    "\t\t1. Nueva Partida  \n"
-	    "\t\t2. Partidas Guardadas \n"
-	    "\t\t3. Cargar Partida \n"
-	    "\t\t4. Ranking \n"
-	    "\t\t5. Instrucciones del Juego \n"
-	    "\t\t6. Salir \n");
-    _fpurge(stdin);
-    scanf(" %i", &opcion);
-    menu(opcion);
+	int opcion;
+	titulo();
+do{
+	switch(opcion = menu){
+		case partida :
+			break; 
+		case guardadas :       
+			break;
+		case cargar :
+			break;
+		case ranking :
+			break; 
+		case instrucciones :
+			break;
+}while(opcion != salir);
 
-
-    return EXIT_SUCCESS;
+return EXIT_SUCCESS;
 }
